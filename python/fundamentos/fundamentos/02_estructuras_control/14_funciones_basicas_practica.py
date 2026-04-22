550c2d07e8cef5516b7d4d2b2b930462541a4f2f
Mabri <mauricioflores@liceovvh.cl>
2026-04-22 14:52:04 -0400

    terminado

--- python/fundamentos/fundamentos/02_estructuras_control/14_funciones_basicas_practica.py
+++ python/fundamentos/fundamentos/02_estructuras_control/14_funciones_basicas_practica.py
@@ -1,3 +1,4 @@
+import os
 #Ejercicio 1
 # Calcula experiencia
 def multiplica_por_2(num):
@@ -5,8 +6,9 @@ def multiplica_por_2(num):
     for i in range(num + 1):
         result.append(i * 2)
     return result
-result1 = multiplica_por_2(5)
-print(result1)
+def resultado1():
+    result1 = multiplica_por_2(5)
+    print(result1)
 # Debe retornar: [0, 2, 4, 6, 8, 10]
 
 #Ejercicio 2
@@ -16,8 +18,9 @@ def suma_y_resta(num):
     print(suma)
     resta = num[0] - num[1]
     return resta
-result2 = suma_y_resta([120, 115])
-print(result2)
+def resultado2():
+    result2 = suma_y_resta([120, 115])
+    print(result2)
 
 # Imprime: 235 y retorna: 5
 
@@ -29,8 +32,9 @@ def sumatoria_menos_longitud(num):
     long = len(num)
     print(f"Longitud: {long}")
     return suma - long
-result3 = sumatoria_menos_longitud([10, 5, 3, 7])
-print(f"Retorno: {result3}")
+def resultado3():
+    result3 = sumatoria_menos_longitud([10, 5, 3, 7])
+    print(f"Retorno: {result3}")
 # Suma total = 25, longitud = 4, debe retornar: 21
 
 #Ejercicio 4
@@ -48,12 +52,13 @@ def valores_multiplicados_segundo(lista):
         print(long)
         return nuevaLista 
     
-result4 = valores_multiplicados_segundo([100, 3, 50, 20])
-print(result4)
-# Imprime: 4 y retorna: [300, 9, 150, 60]
-print()
-print(valores_multiplicados_segundo([100]))
-# Imprime: 1 y retorna: []
+def resultado4():
+    result4 = valores_multiplicados_segundo([100, 3, 50, 20])
+    print(result4)
+    # Imprime: 4 y retorna: [300, 9, 150, 60]
+    print()
+    print(valores_multiplicados_segundo([100]))
+    # Imprime: 1 y retorna: []
 
 #Ejercicio 5
 # Genera precio fijo
@@ -62,8 +67,48 @@ def valor_multiplicado_longitud(valor, longitud):
     for i in range(longitud):
         lista.append(valor * longitud)
     return lista
-print(valor_multiplicado_longitud(5, 2))
-# Debe retornar: [10, 10]
+def resultado5():
+    print(valor_multiplicado_longitud(5, 2))
+    # Debe retornar: [10, 10]
 
-print(valor_multiplicado_longitud(7, 5))
-# Debe retornar: [35, 35, 35, 35, 35]
+    print(valor_multiplicado_longitud(7, 5))
+    # Debe retornar: [35, 35, 35, 35, 35]
+
+def limpiarConsola():
+    os.system('cls')
+
+continuar = True
+while continuar:
+    print("\n ejercicio python: ")
+    print("--- 1.- Ejercicio  1 ---:")
+    print("--- 2.- Ejercicio  2 ---:")
+    print("--- 3.- Ejercicio  3 ---:")
+    print("--- 4.- Ejercicio  4 ---:")
+    print("--- 5.- Ejercicio  5 ---:")
+    opcion = input("\n--- Elige una opción: (1:15) (0 para salir)")
+    if opcion == "1":
+        limpiarConsola()
+        print("\nEjecutando ejercicio 1: ")
+        resultado1()
+    elif opcion == "2":
+        limpiarConsola()
+        print("\nEjecutando ejercicio 2: ")
+        resultado2()
+    elif opcion == "3":
+        limpiarConsola()
+        print("\nEjecutando ejercicio 3: ")
+        resultado3()
+    elif opcion == "4":
+        limpiarConsola()
+        print("\nEjecutando ejercicio 4: ")
+        resultado4()
+    elif opcion == "5":
+        limpiarConsola()
+        print("\nEjecutando ejercicio 5: ")
+        resultado5()
+    elif opcion == 0:
+        limpiarConsola()
+        print("Saliendo...")
+        continuar = False
+    else:
+        print("Opcion no valida, intenta otra vez")
