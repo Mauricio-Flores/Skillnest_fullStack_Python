b48f4ce684b15513a3f3c326333a81f8936ecdd7
Mabri <mauricioflores@liceovvh.cl>
2026-07-14 12:59:33 -0400

    nose

---
+++ python/flask/fundamentos/06.-Motor_plantillas_jinja2/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return render_template("index.html", 
+    nombre = "Mauricio",
+    curso = "4°C",
+    Ciudad = "Santiago",
+    anio = 2026,
+    profesor=False,
+    tecnologias=[
+        "Python",
+        "Flask",
+        "HTML",
+        "CSS"
+    ]
+    )
+
+@app.route("/jugador")
+def jugador():
+    return render_template("jugador.html",
+    jugador="CyberWarrior",
+    puntaje=9200,
+    lider=True)
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
